f4aa31679c10f694b47224cc8b79f2fd8a075f71
darrenrector91 <darrenrector91@gmail.com>
2018-02-05 12:49:54 -0600

    updated but not working spike using google maps for angular

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # solo_project_spike
+
+practice trying to get Google Map API search to work
--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,60 +1,66 @@
-angular.module('map', ['ui.bootstrap', 'ngMap']).controller('ModalDemoCtrl', function ($scope, $modal, $log) {
+var app = angular.module('myApp', []);
 
-    $scope.geopos = {lat:51.50722,lng:-0.12750};
-  
-      
-    $scope.open = function (size) {
-  
-      var modalInstance = $modal.open({
-        templateUrl: 'myModalContent.html',
-        controller: 'ModalInstanceCtrl',
-        size: size,
-        scope: $scope,
-        resolve: {
-          lat: function () {
-            return $scope.geopos.lat;
-          },
-          lng: function () {
-            return $scope.geopos.lng;
-          }
+app.service('Map', function($q) {
+    
+    this.init = function() {
+        var options = {
+            center: new google.maps.LatLng(41.850033, -97.992272),
+            zoom: 13,
+            disableDefaultUI: true    
         }
-      });
-  
-      modalInstance.result.then(function (selectedItem) {
-      }, function () {
-        $log.info('Modal dismissed at: ' + new Date());
-      });
-    };
-  });
-  
-  // Please note that $modalInstance represents a modal window (instance) dependency.
-  // It is not the same as the $modal service used above.
-  
-  angular.module('plunker').controller('ModalInstanceCtrl', function ($scope, $modalInstance, lat, lng) {
-  
-    $scope.geopos.lat = lat;
-    $scope.geopos.lng = lng;
+        this.map = new google.maps.Map(
+            document.getElementById("map"), options
+        );
+        this.places = new google.maps.places.PlacesService(this.map);
+    }
     
-    $scope.render = true;
-    $scope.validation_text = "";
+    this.search = function(str) {
+        var d = $q.defer();
+        this.places.textSearch({query: str}, function(results, status) {
+            if (status == 'OK') {
+                d.resolve(results[0]);
+            }
+            else d.reject(status);
+        });
+        return d.promise;
+    }
     
-    $scope.$on('mapInitialized', function(evt, evtMap) {
-    $scope.map = evtMap;
-    $scope.marker = new google.maps.Marker({position: evt.latLng, map: $scope.map});
-    $scope.click = function(evt) {
-      var latitude = evt.latLng.lat().toPrecision(8);
-      var longitude = evt.latLng.lng().toPrecision(8);
-      $scope.validation_text = "";
-      $scope.marker.setPosition(evt.latLng);
-      $scope.map.panTo(evt.latLng);
-      $scope.map.setZoom(10);
-      $scope.geopos.lat = latitude;
-      $scope.geopos.lng = longitude;         
-      }
-    });
-  
-    $scope.ok = function () {
-      $modalInstance.close();
-    };
-  
-  });
+    this.addMarker = function(res) {
+        if(this.marker) this.marker.setMap(null);
+        this.marker = new google.maps.Marker({
+            map: this.map,
+            position: res.geometry.location,
+            animation: google.maps.Animation.DROP
+        });
+        this.map.setCenter(res.geometry.location);
+    }
+    
+});
+
+app.controller('newPlaceCtrl', function(self, Map) {
+    
+    self.place = {};
+    
+    self.search = function() {
+        self.apiError = false;
+        Map.search(self.searchPlace)
+        .then(
+            function(res) { // success
+                Map.addMarker(res);
+                self.place.name = res.name;
+                self.place.lat = res.geometry.location.lat();
+                self.place.lng = res.geometry.location.lng();
+            },
+            function(status) { // error
+                self.apiError = true;
+                self.apiStatus = status;
+            }
+        );
+    }
+    
+    self.send = function() {
+        alert(self.place.name + ' : ' + self.place.lat + ', ' + self.place.lng);    
+    }
+    
+    Map.init();
+});
